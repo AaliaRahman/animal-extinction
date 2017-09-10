c43f8a5658a3ec4efcfa621b542004803916dc78
AaliaRahman <aaliarahma@gmail.com>
2017-09-09 20:18:46 -0500

    Added background, title and images

--- src/App.js
+++ src/App.js
@@ -2,36 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     const elephant = {
       name:"Sumatran Elephant",
-      imageUrl: "https://www.ecosia.org/images?q=sumatran+elephant"
+      src: "./images/elephant-sample.png",
+      population: "2,400-2,800",
+      status: "Critically Endangered"
     };
     const tiger = {
-      name: "Sumatran Tiger"
+      name: "Sumatran Tiger",
+      population: "400-500",
+      status: "Critically Endangered"
     }
     const orangutan = {
-      name: "Sumatran Orangutan"
+      name: "Sumatran Orangutan",
+      population: "14,613",
+      status: "Critically Endangered"
     }
     const rhino = {
-      name: "Sumatran Rhino"
+      name: "Sumatran Rhino",
+      src: "./images/SumatranRhino.png",
+      population: "<100",
+      status: "Critically Endangered"
   }
 
-  //const animals = ['elephant', 'tiger', 'orangutan', 'rhino'];
+  const animals = ['elephant', 'tiger', 'orangutan', 'rhino'];
 
     return (
-      <div>
+      <div className="container">
 
-        {elephant.name}
-        <br />
-        {tiger.name}
-        <br />
-        {orangutan.name}
-        <br />
-        {rhino.name}
+      <h1>The History and Future of Animal Extinction</h1>
+      <img src="./images/SumatranForest.jpg" />
 
-        <img src="https://www.ecosia.org/images?q=sumatran+forest" />
+        <div className="elephant">
+        <img src={elephant.src} />
+        </div>
+
+
+        <img src={rhino.src}  className="Srhino"/>
+
+        <div className="RhinoInformation">
+        </div>
+
+        <nav>
+        <ul>
+        <li><a href="#">Past</a></li>
+        <li></li>
+        <li><a href="#">Present</a></li>
+        <li></li>
+        <li><a href="#">Future</a></li>
+        </ul>
+        </nav>
+
 
       </div>
 
