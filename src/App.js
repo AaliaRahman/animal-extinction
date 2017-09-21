a786352f8f35c7d50989e2733859991944cd701d
AaliaRahman <aaliarahma@gmail.com>
2017-09-20 20:12:25 -0500

    Attempted at click event. Made progress.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {timePeriod: 'present'};
+   }
+
+  handleClick = () => {
+    console.log("Why isn't this working?")
+    this.setState({timePeriod: 'present'});
+}
+
   render() {
     const elephant = {
       name:"Sumatran Elephant",
@@ -32,78 +42,92 @@ class App extends Component {
     src: "./images/Mammoth.png",
     status: "Currently Extinct"
 }
-const wrhino ={
+const wrhino = {
   name: "Wooly Rhino",
   src: "./images/WoolyRhino.png",
   status: "Currently Extinct"
 }
 
   const animals = ['elephant', 'tiger', 'orangutan', 'rhino'];
+  //const event = document.getElementsByClassName("Past");
 
+  let timePeriod = null;
+  if(this.state.timePeriod === "present") {
+    timePeriod = (
+      <div className="present">
+    <img src="./images/SumatranForest.jpg" />
+
+      <div>
+      <img src={elephant.src} className="Selephant"/>
+      <div className="ElephantInfo">
+      <h2>{elephant.name}</h2>
+      <p>{elephant.status}</p>
+      <p>{elephant.population}</p>
+      </div>
+      </div>
+
+      <div>
+      <img src={rhino.src}  className="Srhino"/>
+      <div className="RhinoInfo">
+      <h2>{rhino.name}</h2>
+      <p>{rhino.status}</p>
+      <p>{rhino.population}</p>
+      </div>
+      </div>
+
+      </div>)
+
+  } else if(this.state.timePeriod === "Past") {
+    timePeriod = (
+    <div className="Past">
+    <img src="./images/IceAge.jpg" />
+
+    <div>
+    <img src={mammoth.src}  className="Selephant"/>
+    <div className="ElephantInfo">
+    <h2>{mammoth.name}</h2>
+    <p>{mammoth.status}</p>
+    <p>{mammoth.population}</p>
+    </div>
+    </div>
+
+    <div>
+    <img src={mammoth.src}  className="Selephant"/>
+    <div className="ElephantInfo">
+    <h2>{mammoth.name}</h2>
+    <p>{mammoth.status}</p>
+    <p>{mammoth.population}</p>
+    </div>
+    </div>
+
+    </div>)
+  } else {
+    timePeriod =(
+      <div></div>
+    )
+  }
 
     return (
       <div className="container">
-
       <h1>The History and Future of Animal Extinction</h1>
+      {timePeriod}
 
-      <div className="present">
-      <img src="./images/SumatranForest.jpg" />
 
-        <div>
-        <img src={elephant.src} className="Selephant"/>
-        <div className="ElephantInfo">
-        <h2>{elephant.name}</h2>
-        <p>{elephant.status}</p>
-        <p>{elephant.population}</p>
-        </div>
-        </div>
 
-        <div>
-        <img src={rhino.src}  className="Srhino"/>
-        <div className="RhinoInfo">
-        <h2>{rhino.name}</h2>
-        <p>{rhino.status}</p>
-        <p>{rhino.population}</p>
-        </div>
-        </div>
-
-        </div>
-
-        <div className="Past">
-        <img src="./images/IceAge.jpg" />
-
-        <div>
-        <img src={mammoth.src}  className="Selephant"/>
-        <div className="ElephantInfo">
-        <h2>{mammoth.name}</h2>
-        <p>{mammoth.status}</p>
-        <p>{mammoth.population}</p>
-        </div>
-        </div>
-
-        <div>
-        <img src={mammoth.src}  className="Selephant"/>
-        <div className="ElephantInfo">
-        <h2>{mammoth.name}</h2>
-        <p>{mammoth.status}</p>
-        <p>{mammoth.population}</p>
-        </div>
-        </div>
-
-        </div>
 
         <nav>
         <ul className="row">
         <li className="col-md-2 col-md-offset-1" onClick={this.handleClick}><a href="#">Past</a></li>
         <li className="col-md-2"><a href="#"><i className="glyphicon glyphicon-minus-sign">•</i></a></li>
-        <li className="col-md-2"><a href="#">Present</a></li>
+        <li className="col-md-2" onClick={this.handleClick}><a href="#">Present</a></li>
         <li className="col-md-2"><a href="#">•</a></li>
-        <li className="col-md-2"><a href="#">Future</a></li>
+        <li className="col-md-2" onClick={this.handleClick}><a href="#">Future</a></li>
         </ul>
         </nav>
 
         <footer>All information was retrieved from World Wildlife Fund. View their website for more information.</footer>
 
+        <div>{this.state.timePeriod}</div>
         </div>
     );
   };
